b06f45aa51f983f7b78fd43aebc4443ecafe2753
AlondraBnn <alondra.ferra@gmail.com>
2024-06-14 20:53:16 -0600

    finished project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Proyecto5.FEMA
-Ferra Muñiz Alondra
+## Ferra Muñiz Alondra
+
+La presente aplicación ofrece la oportunidad de visualizar y analizar información clave sobre vehículos disponibles en el mercado. Con solo unos clics, permite explorar dos tipos de visualizaciones:
+
+- **Histograma de Kilometraje:** Permite observar cómo se distribuyen los vehículos en venta según el kilometraje registrado en su odómetro y da la opción de modificar el histograma en función del tipo de combustible de los vehículos.
+
+- **Gráfico de Dispersión de Kilometraje y Precio:** Muestra la relación entre el kilometraje del odómetro y el precio de los vehículos en venta, además de permitir identificar, a través de diferentes colores, los años de los vehículos anunciados.
--- app.py
+++ app.py
@@ -2,15 +2,51 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+st.header('Proyecto 5. Herramientas de Desarrollo de Software')
+st.subheader('Datos de anuncios de venta de vehículos')
+st.write('¡Bienvenido a la herramienta interactiva para explorar datos de anuncios de venta de vehículos!')
 
-if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
+st.subheader('Instrucciones')
+st.write('Selecciona las opciones para visualizar los gráficos')
+
+car_data = pd.read_csv('vehicles_us.csv') # lee los datos
+build_histogram = st.checkbox('Construir un histograma') #casilla de verificación para crear un histograma
+build_scatter = st.checkbox('Construir gráfico de dispersión') #casilla de verificación para crear un gráfico de dispersión
+
+if build_histogram:  # al hacer clic en la casilla
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
 
-    # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+    # crea un histograma
+    fig_hist = px.histogram(car_data, x="odometer", color="fuel", color_discrete_map={
+        'Gasoline': 'red',
+        'Diesel': 'orange',
+        'Electric': 'green',
+        'Hybrid': 'yellow'
+    })
+
+    # personaliza el diseño del gráfico
+    fig_hist.update_layout(
+        title='Histograma de Vehículos en Venta según Kilometraje',
+        xaxis_title='Kilometraje',
+        yaxis_title='Cantidad de Vehículos'
+    )
+    
+    # muestra un gráfico Plotly interactivo
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+
+if build_scatter: # al hacer click a la casilla
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    #crea un gráfico de dispersión
+    fig_scatter = px.scatter(car_data, x="odometer", y="price", color="model_year", color_continuous_scale='Bluered_r') 
+
+    # personaliza el diseño del gráfico
+    fig_scatter.update_layout(
+        title='Relación entre Kilometraje y Precio de Vehículos',
+        xaxis_title='Kilometraje',
+        yaxis_title='Precio de Vehículos'
+    )
+
+    # muestra un gráfico Plotly interactivo
+    st.plotly_chart(fig_scatter, use_container_width=True)
